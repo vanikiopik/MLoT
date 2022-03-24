a3fb0b61256f062d2c06e33952022aadc03b8b9d
Ivan Kalenikov <vanikiopik@gmail.com>
2022-03-24 14:47:50 +0300

    UPD: made double-jumps & NEW: add run animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer sr;
+
+    // Fields for isGrounded method
+    public bool isGrounded;
+    public Transform groundCheck;
+    private float checkRadius = 0.8f;
+    public LayerMask whatIsGround;
+
+    // Fields for double-jumps
+    public int allowJumps;
+    private int jumpCount = 2;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        jumpCount = 0;
     }
-    
+
+
     void Move()
     {
-        //Stupid realisation of character movement NEED TO FIX
         if (Input.GetAxis("Horizontal") > 0)
         {
             rb.velocity = new Vector2(1f, rb.velocity.y);
+            animator.SetInteger("Dir", 1);  // Sets the animation
         }
         else if (Input.GetAxis("Horizontal") < 0)
         {
             rb.velocity = new Vector2(-1f, rb.velocity.y);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);  // Rotating sprite to other side
+            animator.SetInteger("Dir", -1);
         }
         else
         {
             rb.velocity = new Vector2(0f, rb.velocity.y);
+            animator.SetInteger("Dir", 0);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
 
@@ -32,18 +53,35 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(0f, 4f);
+            // First jump
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(0f, 4f);
+            }
+            // Second jump
+            else if (jumpCount < allowJumps - 1)
+            {
+                rb.velocity = new Vector2(0f, 4f);
+                jumpCount++;
+            }
         }
     }
 
 
     void Update()
     {
-        if (rb.velocity.y == 0)
-        {
-            Jump();
-        }
-
+        Jump();
         Move();
+    }
+
+
+    void FixedUpdate()
+    {
+        //State for checking object collision on the ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
     }
 }
